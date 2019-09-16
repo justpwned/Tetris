8a206026325c4cdd8cf7160ac5762d8d73b75180
justpwned <justpwnedfrom@gmail.com>
2019-09-16 16:03:29 +0500

    Changes in build.bat

--- src/build.bat
+++ src/build.bat
@@ -1,5 +1,17 @@
 @echo off
 
+set arg1=%1
+
+if not exist ..\build mkdir ..\build
+pushd ..\build
+
+if "%arg1%" == "clean" (
+	del *.exe
+	del *.obj
+	del *.pdb
+	del *.cod 
+) ELSE (
+
 set DebugCompilerOptions=-Fetetris.exe -Oi -WX -W4 -EHsc -wd4100 -wd4189 -wd4201 -wd4505 -wd4456 -wd4996 -wd4091 -wd4101 -nologo -Zi -FAsc 
 
 set ReleaseCompilerOptions=-FeTetrisRelease.exe -O2 -WX -W4 -EHsc -wd4100 -wd4189 -wd4201 -wd4505 -wd4456 -wd4996 -nologo -FAsc
@@ -14,9 +26,6 @@ REM ----------------------------------------------------------------------------
 
 set IncludeDirectories=/I "..\dependencies\SDL2_ttf\include" /I "..\dependencies\SDL2\include"
 
-if not exist ..\build mkdir ..\build
-pushd ..\build
-
 set FilesToCompile=..\src\main.cpp ..\src\game.cpp ..\src\graphics\graphics.cpp ..\src\board.cpp ..\src\palette.cpp ..\src\tetromino.cpp ..\src\piece.cpp ..\src\stats.cpp ..\src\menu.cpp ..\src\ui\button.cpp
 
 REM Debug build
@@ -24,6 +33,8 @@ cl %DebugCompilerOptions% %IncludeDirectories% %FilesToCompile% /link %DebugLink
 
 REM Release build
 REM cl %ReleaseCompilerOptions% %IncludeDirectories% %FilesToCompile% /link %ReleaseLinkerOptions%
+
+)
 
 popd
 
